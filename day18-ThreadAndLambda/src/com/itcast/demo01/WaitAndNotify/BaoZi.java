db05ecbdd68e1de317983142531487eba2898760
hanbinq <931703092@qq.com>
2019-05-16 15:58:13 +0800

    2019.05.16-day18

---
+++ day18-ThreadAndLambda/src/com/itcast/demo01/WaitAndNotify/BaoZi.java
@@ -0,0 +1,14 @@
+package com.itcast.demo01.WaitAndNotify;
+/*
+* 资源类：包子类
+* 设置包子的属性
+* 包子的状态：有 true，没有 false
+* */
+public class BaoZi {
+    // 皮
+    String pi;
+    // 陷
+    String xian;
+    // 包子的状态，设置初始值为false没有
+    boolean flag = false;
+}
